f220baa4114cd635d453a8f9c49169d658143395
marinaongithub <73482347+marinaongithub@users.noreply.github.com>
2023-01-20 21:37:59 +0000

    Created object storing questions/options/anwers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,12 +9,14 @@ var choices = document.getElementById("choices");
 
 var timeLeft = 76; 
 
-//when I clcik the start button the timer starts
+// Timer starts when button clicked
 start.addEventListener("click", function() {
     setTime();
-    renderQuestions()
+    initQuestion();
+    nextQuestion();
 })
 
+// Handles the timer
 function setTime() {
     var timeInterval = setInterval(function() {
         timeLeft--;
@@ -27,8 +29,7 @@ function setTime() {
     }, 1000)
 }
 
-// and I am presented with questions
-function renderQuestions() {
+function initQuestion() {
 
     // remove start screen
     startScreen.textContent = "";
@@ -37,7 +38,7 @@ function renderQuestions() {
     questionDiv.setAttribute("class", "show");
 
     // populate with current question
-    question.textContent = questions[0];
+    question.textContent = questions[0];          // to change
 
     // populate list of choices
 
@@ -57,6 +58,44 @@ function renderQuestions() {
         ulTag.appendChild(buArr[i]);
         buArr[i].textContent = responses[i];
     }
+
+
+}
+
+// Render the question + choices
+function nextQuestion() {
+
+    // remove start screen
+    startScreen.textContent = "";
+
+    // show questions 
+    questionDiv.setAttribute("class", "show");
+
+    // populate with current question
+    question.textContent = questions[0];          // to change
+
+    // populate list of choices
+
+        // creates ul tag
+    ulTag = document.createElement("ul");
+
+    bu1 = document.createElement("button");
+    bu2 = document.createElement("button");
+    bu3 = document.createElement("button");
+    bu4 = document.createElement("button");
+
+        // array to store all the button tag 
+    var buArr = [bu1, bu2, bu3, bu4];
+
+        // loops through the buttons array to append each of them to the ul tag with content
+    for (var i = 0; i < responses.length; i++) {
+        ulTag.appendChild(buArr[i]);
+        buArr[i].textContent = responses[i];
+    }
+
+        // when I answer a question then I am presented with another question
+
+
 
 }
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,35 @@
-var questions = ["Commonly used data types DO NOT include:"];
+var quizz = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts",
+    },
 
-var responses = ["1. strings", "2. booleans", "3. alerts", "4. numbers"];
+    {
+        question: "The condition in an if/else statement is enclosed within ______.",
+        options: ["1. quotes", "2. curly brakets", "3. parenthesis", "4. square brackets"],
+        answer: "3. parentheses",
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above",
+
+    },
+
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables",
+        options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        answer: "3. quotes",
+
+    },
+
+    {
+        question: "A very useful tool during development and debugging for printing content to the debugger:",
+        options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        answer: "4. console.log",
+
+    },
+]
+
